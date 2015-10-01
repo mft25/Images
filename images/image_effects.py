2e2e80caed062a345529f348142716789830cefb
matt <matt@tune.zone>
2015-10-01 23:59:36 +0100

    Minor improvements

--- images/image_effects.py
+++ images/image_effects.py
@@ -12,7 +12,7 @@ class ImageEffects(image.Image):
 		
 	def _pixelate(self, i, j, pixel_size, setter, *args, **kwargs):
 		if j % pixel_size == 0 and i % pixel_size == 0:
-			pixel_aggregate = [0, 0, 0, 0x0]
+			pixel_aggregate = [0, 0, 0, 0]
 			count = 0
 			for k in xrange(0, pixel_size**2):
 				pixel = self.get_pixel(i + k%pixel_size, j + k/pixel_size)
@@ -20,13 +20,9 @@ class ImageEffects(image.Image):
 					pixel_aggregate = [sum(x) for x in zip(pixel_aggregate, pixel)]
 					count = count + 1
 			new_pixel = [pixel_aggregate[0]/count, pixel_aggregate[1]/count, pixel_aggregate[2]/count, 0xff]
-			jitter_x = 0
-			jitter_y = 0
 			if kwargs and 'jitter' in kwargs:
-				jitter_x = random.randint(-kwargs['jitter'], kwargs['jitter'])
-				jitter_y = random.randint(-kwargs['jitter'], kwargs['jitter'])
-				args = args + (jitter_x,)
-				args = args + (jitter_y,)
+				args = args + (random.randint(-kwargs['jitter'], kwargs['jitter']),)
+				args = args + (random.randint(-kwargs['jitter'], kwargs['jitter']),)
 			for k in xrange(0, pixel_size**2):
 				setter(i + k%pixel_size, j + k/pixel_size, new_pixel, *args)
 
@@ -35,12 +31,12 @@ class ImageEffects(image.Image):
 			self.set_pixel(i, j, pixel)
 		self.for_all_pixels(self._pixelate, pixel_size, _standard_pixelator)
 
-	def pointillism(self, radius, buffer=0, jitter=0, overwrite_img=True):
+	def pointillism(self, radius, buffer=0, jitter=0):
 		def _pointillism(i, j, pixel, radius, buffer, jitter_x, jitter_y):
 			diameter = 2*radius
 			if (radius - i%diameter)**2 + (radius - j%diameter)**2 < (radius-buffer)**2:
 				self.set_pixel(i + jitter_x, j + jitter_y, pixel, True)
 		self.new_canvas()
 		self.for_all_pixels(self._pixelate, 2*radius, _pointillism, radius, buffer, jitter=jitter)
-		self.img = self.canvas if overwrite_img else self.img
+		self.img = self.canvas
 
